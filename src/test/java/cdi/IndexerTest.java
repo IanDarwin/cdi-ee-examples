8722229a261a5dafbda60950095e52625cbe53dc
Ian Darwin <ian@darwinsys.com>
2019-04-20 16:17:37 -0400

    Build test more

--- src/test/java/cdi/IndexerTest.java
+++ src/test/java/cdi/IndexerTest.java
@@ -1,11 +1,30 @@
 package cdi;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
+import java.util.Map;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class IndexerTest {
-
+	
+	private Indexer indexer;
+	
+	@Before
+	public void setup() {
+		indexer = new Indexer();
+	}
+	
+	public void testIndexer() {
+		Article a = new Article();
+		a.title = "Hello world";
+		indexer.updateIndex(new ArticleEvent(ArticleEvent.Type.NEW, a));
+		Map<String, Article> map = indexer.getIndex();
+		assertEquals(2, map.size());
+	}
 	@Test
 	public void testIsStopWord() {
 		assertTrue(Indexer.isStopWord("in"));
