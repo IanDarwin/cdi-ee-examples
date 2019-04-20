e00125d65268c2044ca36c6426c6f47d65e88982
Ian Darwin <ian@darwinsys.com>
2019-04-20 18:43:45 -0400

    Stop words a bit better

--- src/main/java/cdi/Indexer.java
+++ src/main/java/cdi/Indexer.java
@@ -23,9 +23,24 @@ public class Indexer {
 		System.out.println("Indexer.updateIndex(): INDEXING " + article);
 		
 		// Do the indexing (a real app would make this persistent, but oh well).
-		for (String s : article.title.split("\\s")) {
-			map.put(s.toLowerCase(), article);
+		for (String s : article.title.toLowerCase().split("\\s")) {
+			if (!isStopWord(s)) {
+				map.put(s.toLowerCase(), article);
+			}
 		}
+
+		// XXX Later: add indexing of body
+	}
+
+	/** Check for stop words (words not to index).
+	 * ASSERT: Input is in lower case already
+	 * @return True if input is a stopword, false otherwise.
+	 */
+	static boolean isStopWord(String s) {
+		if (s.length() == 1) {
+			return false;
+		}
+		return s.equals("an") || s.equals("in") || s.equals("on") || s.equals("to");
 	}
 	
 	public Map<String,Article> getIndex() {
